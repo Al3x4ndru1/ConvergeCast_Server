4cea9b8e53dd5d07890e5bcf871ee6cb2f500a89
Al3x4ndru1 <alexandruilie309@yahoo.com>
2022-08-10 20:58:57 +0100

    $msg

--- scripts/Backend/get_video.py
+++ scripts/Backend/get_video.py
@@ -18,7 +18,6 @@ environ["QT_DEVICE_PIXEL_RATIO"] = "0"
 environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
 environ["QT_SCREEN_SCALE_FACTORS"] = "1"
 environ["QT_SCALE_FACTOR"] = "1"
-#def show(self,frame):
 
 class ShowClass:
 
